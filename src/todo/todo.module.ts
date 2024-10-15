19df962570f5923ca12b32be21fec8cafd94f116
ggugg <gguggong@naver.com>
2024-10-15 13:43:29 +0900

    fix: todo service 잘못된 폴더 위치 수정

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
-import { TodoService } from './controllers/services/todo.service';
 import { TodoController } from './controllers/todo.controller';
+import { TodoService } from './services/todo.service';
 
 @Module({
   controllers: [TodoController],

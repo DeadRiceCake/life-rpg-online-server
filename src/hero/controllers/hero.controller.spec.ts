72c213d7b0594a3b89aa59b43d99b9f53551f36a
DeadRiceCake <gguggong@naver.com>
2024-10-21 00:38:25 +0900

    test: hero controller 테스트 오류 수정

--- src/hero/controllers/hero.controller.spec.ts
+++ src/hero/controllers/hero.controller.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { AppLogger } from '../../shared/logger/logger.service';
+import { HeroService } from '../services/hero.service';
 import { HeroController } from './hero.controller';
 
 describe('HeroController', () => {
   let controller: HeroController;
 
+  const mockedHeroService = {
+    getHeroByUserId: jest.fn(),
+  };
+
+  const mockedLogger = { setContext: jest.fn(), log: jest.fn() };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HeroController],
+      providers: [
+        { provide: HeroService, useValue: mockedHeroService },
+        { provide: AppLogger, useValue: mockedLogger },
+      ],
     }).compile();
 
     controller = module.get<HeroController>(HeroController);
